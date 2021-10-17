f3bcd64969a16ea98c35e4dfd1fc683f4846f998
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2021-10-17 12:19:22 -0300

    clean code e login

---
+++ src/componentes/Login.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Login.css';
+import '../Global.css'
+function Login(){
+    return(
+        <div className="container">
+            <div className="login">
+                <div className="row">
+                    <h1>Login</h1>
+                </div>
+                <div className="row">
+                    <input></input>
+                </div>                
+            </div>
+        </div>
+    );    
+}
+
+
+export default Login;
