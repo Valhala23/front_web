a7b786f3b732f5936de7d204127ea86328864bd0
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2021-10-31 14:27:56 -0300

    estilo cadlogin

--- src/componentes/CadastroLogin.jsx
+++ src/componentes/CadastroLogin.jsx
@@ -3,17 +3,16 @@ import './estilos/CadastroLogin.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 function CadLogin(){
     return(        
-        <div className="container" style={{backgroundColor: '#d3d3da', weigth: '90%', marginRight: '5%' }}>
-            <div className="cadstrolog">            
-                <div className="row">  
-                    <div className="col">
-                        <div className="menucad">
-                            <h1>Cadastro</h1>
-                            <h2>Login</h2>
-                        </div>  
-                    </div>              
-                </div>
-
+        <div className="cadstrolog">            
+            <div className="row">  
+                <div className="col">
+                    <div className="menucad">
+                        <h1>Cadastro</h1>
+                        <h2>Login</h2>
+                    </div>  
+                </div>              
+            </div>
+            <form action="">
                 <div className="row">
                     <div className="col">
                         <p>nome</p>
@@ -27,7 +26,7 @@ function CadLogin(){
                         <p>acesso</p>
                     </div>
                 </div>
-            </div>
+            </form>
         </div>
     );    
 }
