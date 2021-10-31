6fbaf043d30d67fbe4246ffe53ad82536f1402f3
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2021-10-31 15:46:27 -0300

    estilo login

--- src/componentes/CadastroLogin.jsx
+++ src/componentes/CadastroLogin.jsx
@@ -14,16 +14,36 @@ function CadLogin(){
             </div>
             <form action="">
                 <div className="row">
-                    <div className="col">
+                    <div className="col-4">                        
                         <p>nome</p>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col">
-                        <p>senha</p>
                     </div>
                     <div className="col">
                         <p>acesso</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-4">
+                        <input type="text" />
+                    </div>
+                    <div className="col">
+                        <input type='radio' name='group' value='admin' /> Administrador                         
+                    </div>
+                    <div className="col">
+                        <input type='radio' name='group' value='user' />  Usúario 
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-4">
+                        <p>senha</p>
+                        <input type="password" />
+                    </div>
+                </div>
+                <div className="row">
+
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <button type="button" class="btn btn-secondary">Cadastrar</button>                        
                     </div>
                 </div>
             </form>
