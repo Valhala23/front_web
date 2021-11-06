8fc264f1abe67e5a5fa36d6c4e69a03089527979
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2021-11-06 16:29:45 -0300

    cadastro estilo

--- src/componentes/CadastroLogin.jsx
+++ src/componentes/CadastroLogin.jsx
@@ -13,6 +13,7 @@ function CadLogin(){
         id: '',
         nome: '',
         senha: '',
+        ApelidoLogin: '',
         permissao: ''
     });
 
@@ -41,44 +42,55 @@ function CadLogin(){
     return(        
         <div className="cadstrolog">            
             <div className="row">  
-                <div className="col">
+                <div className="col-4">
                     <div className="menucad">
                         <h1>Cadastro</h1>
                         <h2>Login</h2>
                     </div>  
                 </div>              
             </div>
-            <div className="row">
-                <div className="col">
-                    <p>Nome</p>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-6">
-                    <input type="text" name="nome" onChange={handleChange} />
-                </div>
-            </div>
-            <div className="row">
-                <div className="col">
-                    <p>Senha</p>
-                </div>
-                <div className="col">
-                    <p>Acesso</p>
-                </div>
-                <div className="row">
-                    <div className="col-6">
-                        <input type="password" name="senha" onChange={handleChange} />
-                    </div>
-                    <div className="col-1">
-                        <input type="radio" name="permissao" value="0" onChange={handleChange}/>
-                    </div>
-                    <div className="col-1">
-                        <input type="radio" name="permissao" value="1" onChange={handleChange}/>
-                    </div>
-                </div>
+            <div className="formulario">
                 <div className="row">
                     <div className="col">
-                        <button style={{float: 'right'}} type="button" className="btn btn-secondary" onClick={()=> usuarioPost()}>Cadastrar</button>
+                        <label>Nome</label>
+                    </div>
+                    <div className="col-4">
+                    </div>
+                    <div className="col">
+                    </div>
+                </div>            
+                <div className="row">
+                    <div className="col-4">
+                        <input type="text" name="nome" onChange={handleChange} />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-4">
+                        <label>Apelido</label>
+                    </div>
+                    <div className="col-4">
+                        <label>Senha</label>
+                    </div>
+                    <div className="col">
+                        <label>Acesso</label>
+                    </div>
+                    <div className="row">
+                    <div className="col-4">
+                            <input type="text" name="ApelidoLogin" onChange={handleChange} />
+                        </div>
+                        <div className="col-4">
+                            <input type="password" name="senha" onChange={handleChange} />
+                        </div>
+                        <div className="col-1">
+                            <input type="radio" name="permissao" value="0" onChange={handleChange}/>Adm
+
+                            <input type="radio" name="permissao" value="1" onChange={handleChange}/>User
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <button style={{float: 'right'}} type="button" className="btn btn-secondary" onClick={()=> usuarioPost()}>Cadastrar</button>
+                        </div>
                     </div>
                 </div>
             </div>
