b2610e5d91f985d02cd6072fe127970fdc9df6f9
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2021-11-06 15:57:04 -0300

    cadastrando login okei

--- src/componentes/CadastroLogin.jsx
+++ src/componentes/CadastroLogin.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './estilos/CadastroLogin.css'
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function CadLogin(){
+
+    const baseUrl ="https://localhost:44390/api/Usuario";
+
+    const [data, setData]=useState([]);
+
+    const [usuariolog, setUsuariolog]=useState(
+    {
+        id: '',
+        nome: '',
+        senha: '',
+        permissao: ''
+    });
+
+    const handleChange = e=> 
+    {
+        // Montar objeto usuario
+        const {name, value}=e.target;
+        setUsuariolog(
+        {
+            ...usuariolog,
+            [name]: value
+    });
+        console.log(usuariolog);
+    }   
+    
+    const usuarioPost = async()=>{
+        delete usuariolog.id;
+        await axios.post(baseUrl, usuariolog)
+        .then(response => {
+          setData(response.data);
+        }).catch(error=> {
+          console.log(error);
+        })
+      }    
+    
     return(        
         <div className="cadstrolog">            
             <div className="row">  
@@ -12,42 +48,40 @@ function CadLogin(){
                     </div>  
                 </div>              
             </div>
-            <form action="">
-                <div className="row">
-                    <div className="col">
-                        <p>Nome</p>
-                    </div>
+            <div className="row">
+                <div className="col">
+                    <p>Nome</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-6">
+                    <input type="text" name="nome" onChange={handleChange} />
+                </div>
+            </div>
+            <div className="row">
+                <div className="col">
+                    <p>Senha</p>
+                </div>
+                <div className="col">
+                    <p>Acesso</p>
                 </div>
                 <div className="row">
                     <div className="col-6">
-                        <input type="text" id="log" name="log" />
+                        <input type="password" name="senha" onChange={handleChange} />
+                    </div>
+                    <div className="col-1">
+                        <input type="radio" name="permissao" value="0" onChange={handleChange}/>
+                    </div>
+                    <div className="col-1">
+                        <input type="radio" name="permissao" value="1" onChange={handleChange}/>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col">
-                        <p>Senha</p>
-                    </div>
-                    <div className="col">
-                        <p>Acesso</p>
-                    </div>
-                    <div className="row">
-                        <div className="col-6">
-                            <input type="password" name="senha" id="senha" />
-                        </div>
-                        <div className="col-1">
-                            <input type="radio" id="louie" name="drone" value="louie"/>
-                        </div>
-                        <div className="col-1">
-                            <input type="radio" id="louie" name="drone" value="louie"/>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col">
-                            <button style={{float: 'right'}} type="button" class="btn btn-secondary">Cadastrar</button>
-                        </div>
+                        <button style={{float: 'right'}} type="button" className="btn btn-secondary" onClick={()=> usuarioPost()}>Cadastrar</button>
                     </div>
                 </div>
-            </form>
+            </div>
         </div>
     );    
 }
