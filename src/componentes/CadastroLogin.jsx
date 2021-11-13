0bb138dc99c46bb20e6caca84f3fbf085a54f77b
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2021-11-13 00:01:52 -0300

    cadastro log

--- src/componentes/CadastroLogin.jsx
+++ src/componentes/CadastroLogin.jsx
@@ -26,7 +26,6 @@ function CadLogin(){
             ...usuariolog,
             [name]: value
     });
-        console.log(usuariolog);
     }   
     
     const usuarioPost = async()=>{
@@ -41,50 +40,47 @@ function CadLogin(){
     
     return(        
         <div className="cadstrolog">            
-            <div className="row">  
-                <div className="col-4">
-                    <div className="menucad">
-                        <h1>Cadastro</h1>
-                        <h2>Login</h2>
-                    </div>  
-                </div>              
+            <div className="menucad">
+                <h1>Cadastro</h1>
+                <h2>Login</h2>
             </div>
             <div className="formulario">
-                <div className="row">
-                    <div className="col">
-                        <label>Nome</label>
-                    </div>
-                    <div className="col-4">
-                    </div>
-                    <div className="col">
-                    </div>
-                </div>            
-                <div className="row">
-                    <div className="col-4">
-                        <input type="text" name="nome" onChange={handleChange} />
-                    </div>
+                <div className="card">   
+                    <div className="card-body">
+                        <div className="row">
+                            <div className="col">
+                                <label>Acesso</label>
+                            </div>
+                            </div>
+                            <div className="row">
+                                <div className="col">
+                                    <input type="radio" name="permissao" value="0" onChange={handleChange}/>Adm
+                                    <input type="radio" name="permissao" value="1" onChange={handleChange}/>User
+                                </div>
+                        </div>
+                    </div>                                          
                 </div>
-                <div className="row">
-                    <div className="col-4">
-                        <label>Apelido</label>
-                    </div>
-                    <div className="col-4">
-                        <label>Senha</label>
-                    </div>
-                    <div className="col">
-                        <label>Acesso</label>
+                <div className="camposcad">
+                    <div className="row">
+                        <div className="col">
+                            <label>Nome</label>
+                            <input type="text" name="nome" onChange={handleChange} />
+                        </div>
                     </div>
                     <div className="row">
-                    <div className="col-4">
+                        <div className="col">
+                            <label>Apelido</label>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
                             <input type="text" name="ApelidoLogin" onChange={handleChange} />
                         </div>
-                        <div className="col-4">
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                        <label>Senha</label>
                             <input type="password" name="senha" onChange={handleChange} />
-                        </div>
-                        <div className="col-1">
-                            <input type="radio" name="permissao" value="0" onChange={handleChange}/>Adm
-
-                            <input type="radio" name="permissao" value="1" onChange={handleChange}/>User
                         </div>
                     </div>
                     <div className="row">
@@ -92,8 +88,8 @@ function CadLogin(){
                             <button style={{float: 'right'}} type="button" className="btn btn-secondary" onClick={()=> usuarioPost()}>Cadastrar</button>
                         </div>
                     </div>
-                </div>
-            </div>
+                </div>                
+            </div>            
         </div>
     );    
 }
--- src/componentes/Login.jsx
+++ src/componentes/Login.jsx
@@ -1,14 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './estilos/Login.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logarimg from '../assets/noun_portal.png';
 import { Link } from 'react-router-dom'
+import axios from 'axios';
 
-class Login extends React.Component {
-    cadLogin() {
-        console.log('Cadastro login');        
-    }
-    render() {
+export default function Login(){
+
+    const baseUrl ="https://localhost:44390/api/Usuario/fazerlogin";    
+
+    const [data, setData]=useState([]);
+
+    const [usuariolog, setUsuariolog]=useState(
+    {
+        id: '',
+        ApelidoLogin: '',
+        senha: ''
+    });
+
+    const handleChange = e=> 
+    {
+        // Montar objeto usuario
+        const {name, value}=e.target;
+        setUsuariolog(
+        {
+            ...usuariolog,
+            [name]: value
+    });
+    }   
+    
+    const usuarioPost = async()=>{
+        delete usuariolog.id;
+        await axios.post(baseUrl, usuariolog)
+        .then(response => {
+          setData(response.data);
+        }).catch(error=> {
+          console.log(error);
+        })
+      } 
         return(        
         <div className="login">
             <div className="imagemlogin img-fluid">
@@ -16,19 +45,17 @@ class Login extends React.Component {
             </div>
 
             <div class="camposlogin form-group"> 
-                <input type="text" placeholder="Login"></input>                
-                <input type="password" placeholder="Senha" />
+                <input type="text" placeholder="Login" name="ApelidoLogin" onChange={handleChange}></input>                
+                <input type="password" placeholder="Senha" name="senha" onChange={handleChange} />
             </div>
             <div className="btnconfirma">
-                <button class="button" class="btn btn-secondary btn-sm" type="submit">Logar</button>
+                <button class="button" class="btn btn-secondary btn-sm" onClick={()=> usuarioPost()}>Logar</button>
             </div>
             <div className="btncadastrp">
                 <Link to="/CadLogin" class="btn btn-info" >Cadastrar login</Link>
             </div>
         </div>
         );    
-    }
 }
 
 
-export default Login;
