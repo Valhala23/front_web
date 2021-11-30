1a2b7bad05f622dfee3607c6da3f6c73c558f201
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2021-11-29 23:58:47 -0300

    estilos pagina perfil

--- src/componentes/Perfil.jsx
+++ src/componentes/Perfil.jsx
@@ -11,21 +11,74 @@ function Perfil(props){
 
     return(
         <div>
-            <NavLink to="/" activeClassName="active">
-                Go Back
-            </NavLink>
+            <div className="cabecalho">
+                <h1>Perfil usuário </h1>
+            </div>
+                
+            <div className="foto">
+                <h2>foto: {location.usuario.foto} </h2>
+            </div>
+                    
+            <div className="container">    
+                <div className="informacoes">
+                    <div className="row">
+                        <div className="col">
+                            <h2>Nome: {location.usuario.nome} </h2>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                                <h2>Apelido: {location.usuario.apelidoLogin} </h2>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <h2>Bio: {location.usuario.bio} </h2>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <h2>Descrição: {location.usuario.descricao} </h2>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <h2>sexo: {location.usuario.sexo} </h2>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col">
+                                <h2>historico: {location.usuario.historico} </h2>
+                            </div>
+                        </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <div className="contato">
+                            <h2>contato: {location.usuario.contato} </h2>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
 
-            <h1>Perfil usuário </h1>
-            <h2>Nome: {location.usuario.nome} </h2>
-            <h2>Apelido: {location.usuario.apelidoLogin} </h2>
-            <h2>Bio: {location.usuario.bio} </h2>
-            <h2>Descrição: {location.usuario.descricao} </h2>
-            <h2>sexo: {location.usuario.sexo} </h2>
-            <h2>historico: {location.usuario.historico} </h2>
-            <h2>contato: {location.usuario.contato} </h2>
-            <h2>foto: {location.usuario.foto} </h2>
-            <h2>permissao: {location.usuario.permissao} </h2>
-            <h2>Token: {console.log(localStorage.getItem('token'))} </h2>            
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <div className="nivel">
+                            <h2>permissao: {location.usuario.permissao} </h2>
+                        </div>
+                    </div>
+                </div>
+                <h2>Token: {console.log(localStorage.getItem('token'))} </h2>            
+
+                <div className="row">
+                    <section className="link">
+                    <NavLink to="/" activeClassName="active">
+                            Go Back
+                    </NavLink>
+                    </section>
+                </div>
+            </div>
         </div>
     );    
 }
