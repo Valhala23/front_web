d7f7b993a693601457da2f46a247ecc8de93bc26
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2022-01-18 23:59:57 -0300

    edicao placeholder ao inves de value para predefifino sugestao

--- src/componentes/EditarPerfil2.jsx
+++ src/componentes/EditarPerfil2.jsx
@@ -3,8 +3,48 @@ import { useLocation } from "react-router-dom";
 import './estilos/Perfil.css'
 import { NavLink } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import api from '../servicos/api'
 
 const EditarPerfil2 = (props) =>{ 
+
+    const [data, setData]=useState([]);
+    const [usuariolog, setUsuariolog]=useState(
+        {
+          id: 0,
+          nome: '',
+          apelidoLogin: '',
+          senha: '',
+          permissao: 0
+        }
+    );
+    
+    const handleChange = e=> 
+    {
+        // Montar objeto usuario
+        const {name, value}=e.target;
+        setUsuariolog(
+        {
+            ...usuariolog,
+            [name]: value
+        });        
+    } 
+
+    const editarUsuario = async()=>{
+        // com o objeto usuario recebido pegar id e enviar para o metodo update                
+        // location.state.usuario.id
+        try {
+            await api.post('https://localhost:44390/api/Usuario/cadastrarnovo', usuariolog)
+            .then(async response => {
+              setData(response.data);
+              //history.push('/');
+            }).catch(error=> {
+              console.log(error);
+            })            
+        } catch (error) {
+            //history.push('/');
+        }
+      } 
+
     
     const location = useLocation();      
     return(
@@ -23,7 +63,7 @@ const EditarPerfil2 = (props) =>{
                     <div className="col-6">
                         <h2>Nome: </h2>
                         {/* onChange={handleChange} */}
-                        <input type="text" value={location.state.usuario.nome} name="nome"  />                        
+                        <input type="text" placeholder={location.state.usuario.nome} name="nome" onChange={handleChange}  />                        
                     </div>
 
                     <div className="col">
@@ -38,32 +78,32 @@ const EditarPerfil2 = (props) =>{
                     <div className="row">
                         <div className="col">
                             <h2>Bio: </h2>
-                            <input type="text" value={location.state.usuario.bio} name="nome"  />
+                            <input type="text" placeholder={location.state.usuario.bio} name="bio"  onChange={handleChange} />
                         </div>
                     </div>
                     <div className="row">
                         <div className="col">
                             <h2>Descrição: </h2>
-                            <input type="text" value={location.state.usuario.descricao} name="nome"  />
+                            <input type="text" placeholder={location.state.usuario.descricao} name="descricao"  onChange={handleChange}/>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col">
                             <h2>sexo: </h2>
-                            <input type="text" value={location.state.usuario.sexo} name="nome"  />
+                            <input type="text" placeholder={location.state.usuario.sexo} name="sexo"  onChange={handleChange} />
                         </div>
                     </div>
                     <div className="row">
                         <div className="col">
                             <h2>historico: </h2>
-                            <input type="text" value={location.state.usuario.historico} name="nome"  />
+                            <input type="text" placeholder={location.state.usuario.historico} name="historico"  onChange={handleChange} />
                         </div>
                     </div>                
                     <div className="row">
                         <div className="col">
                             <div className="contato">
                                 <h2>contato: </h2>
-                                <input type="text" value={location.state.usuario.contato} name="nome"  />
+                                <input type="text" placeholder={location.state.usuario.contato} name="contato"  onChange={handleChange} />
                             </div>
                         </div>
                     </div>
@@ -71,8 +111,13 @@ const EditarPerfil2 = (props) =>{
                         <div className="col">
                             <div className="nivel">
                                 <h2>permissao: {location.state.usuario.permissao} </h2>
-                                <input type="text" value={location.state.usuario.permissao} name="nome"  />
+                                <input type="text" placeholder={location.state.usuario.permissao} name="permissao"  onChange={handleChange} />
                             </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <button style={{float: 'right'}} type="button" className="btn btn-secondary" onClick={()=> editarUsuario()}>Cadastrar</button>
                         </div>
                     </div>
                 </div>
