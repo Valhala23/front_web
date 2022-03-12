a0ed9607c173a26545e2cf087dfd55409e8343b4
Ricardo Bavaresco <ricardo.bav17@gmail.com>
2022-03-12 00:32:38 -0300

    seleção do detalhamento do artigo

--- src/componentes/DetalheArtigo.jsx
+++ src/componentes/DetalheArtigo.jsx
@@ -26,33 +26,49 @@ function Detalhe(){
             <div className="container">
                 <div className="row">
                     <div className="col">
-                        <h1> Lista de Artigos publicados </h1>
+                        <h1> Detalhe do artigo </h1>
                     </div>
                 </div>
-                <div className="row">
-                    <table className="table table-bordered">
-                        <thead>
-                            <tr>
-                            <th>Código</th> <th>Titulo</th> <th>Descrição</th> <th>Detalhar</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {data.map(artigo=> (
-                                <tr key={artigo.codigo}>
-                                <td> {artigo.codigo }</td>
-                                <td> {artigo.titulo }</td>
-                                <td> {artigo.descricao }</td>
-                                <td><Link to="/Publicar" class="btn btn"> Ver</Link></td>
-                            </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                <div className="row">                     
+                    {data.map(artigo=> (
+                        <tr key={artigo.codigo}>
+                            <td> {artigo.codigo }</td>
+                            <td> {artigo.titulo }</td>
+                            <td> {artigo.descricao }</td>
+                        </tr>       
+                    ))}                                                                  
                 </div>
                 
-                <div className="div">
-                    {/* <button className="btn btn-danger" onClick={postarNovo}> Publicar novo </button> */}
-                    <Link to="/Publicar" class="btn btn-info" >Publicar novo</Link>
+                <div className="row">
+                    <div className="col">
+                        <h1>Imagem 1 </h1>
+                    </div>
+
+                    <div className="col">
+                        <p>comentarios ao lado </p>
+                    </div>
                 </div>
+
+                <div className="row">
+                    <div className="col">
+                        <h3>Imagem 2 </h3>
+                    </div>
+
+                    <div className="col">
+                        <p>comentarios ao lado </p>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col">
+                        <h3>Imagem 3 </h3>
+                    </div>
+
+                    <div className="col">
+                        <p>comentarios ao lado </p>
+                    </div>
+                </div>
+                
 
             </div>
 
